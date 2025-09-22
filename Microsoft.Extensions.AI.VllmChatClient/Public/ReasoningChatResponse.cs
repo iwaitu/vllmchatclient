d44f40e713c00854291494a5e8b059b483ebaf1f
iwaitu <iwaitu@aliyun.com>
2025-09-23 07:28:19 +0800

    添加 ReasoningChatResponse 类
    新增一个继承自 ChatResponse 的 ReasoningChatResponse 类。该类包含一个构造函数，接受 ChatMessage 和推理字符串，并确保推理字符串不为 null。还定义了一个只读属性 Reason，用于存储推理信息。

---
+++ Microsoft.Extensions.AI.VllmChatClient/Public/ReasoningChatResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.AI
+{
+    public class ReasoningChatResponse: ChatResponse
+    {
+        public ReasoningChatResponse(ChatMessage message, string reasoning)
+            : base(message)
+        {
+            Reason = reasoning ?? throw new ArgumentNullException(nameof(reasoning));
+        }
+        public string Reason { get; }
+    }
+}
